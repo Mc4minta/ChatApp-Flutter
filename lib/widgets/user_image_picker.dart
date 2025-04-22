63c08ecac3832ffbf9667cc843252ad4377e56ec
PichamonM (MiN) <mintaconan4826@gmail.com>
2025-04-22 18:33:16 +0700

    add pick from gallery function

--- lib/widgets/user_image_picker.dart
+++ lib/widgets/user_image_picker.dart
@@ -4,7 +4,7 @@ import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
 class UserImagePicker extends StatefulWidget {
-  const UserImagePicker({super.key});
+  const UserImagePicker({Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
@@ -15,16 +15,49 @@ class UserImagePicker extends StatefulWidget {
 class _UserImagePickerState extends State<UserImagePicker> {
   File? _pickedImageFile;
 
-  void _pickImage () async {
-    final pickedImage = await ImagePicker().pickImage(source: ImageSource.camera,imageQuality: 50, maxWidth: 150,);
-    if(pickedImage == null){
+  void _pickImage() async {
+    final imageSource = await showDialog<ImageSource>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Select Image Source"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.camera),
+                title: Text('Take a picture'),
+                onTap: () => Navigator.of(context).pop(ImageSource.camera),
+              ),
+              ListTile(
+                leading: Icon(Icons.image),
+                title: Text('Pick from gallery'),
+                onTap: () => Navigator.of(context).pop(ImageSource.gallery),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+
+    if (imageSource == null) {
+      return; // User canceled
+    }
+
+    final pickedImage = await ImagePicker().pickImage(
+      source: imageSource,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
+
+    if (pickedImage == null) {
       return;
     }
+
     setState(() {
       _pickedImageFile = File(pickedImage.path);
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -36,11 +69,14 @@ class _UserImagePickerState extends State<UserImagePicker> {
           foregroundImage: _pickedImageFile != null ? FileImage(_pickedImageFile!) : null,
         ),
         TextButton.icon(
-          onPressed: _pickImage, 
-          icon : Icon(Icons.image),
-          label: Text('Add Image', style: TextStyle(
-            color : Theme.of(context).colorScheme.primary,
-          ),),
+          onPressed: _pickImage,
+          icon: Icon(Icons.image),
+          label: Text(
+            'Add Image',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
         ),
       ],
     );
