bf042943758a47d00134586b1c11c4172754b292
PichamonM (MiN) <mintaconan4826@gmail.com>
2025-04-22 13:53:23 +0700

    Add ChatScreen and Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:chatapp/screens/chat.dart';
+import 'package:chatapp/screens/splash.dart';
 import 'package:chatapp/screens/auth.dart';
-
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 
-
 // const Color.fromARGB(255, 63, 17, 177)
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await FirebaseAppCheck.instance.activate(
     webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
@@ -32,10 +30,23 @@ class App extends StatelessWidget {
       title: 'FlutterChat',
       theme: ThemeData().copyWith(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 17, 92, 177)),
+          seedColor: const Color.fromARGB(255, 17, 92, 177),
+        ),
       ),
-      home: const AuthScreen(),
-      
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
+
+          return const AuthScreen();
+        },
+      ),
     );
   }
 }
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
-  const ChatScreen({super.key});
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('FlutterChat')),
-      body: const Center(child: Text('Logged in!')),
+      body: const Center(child: Text('Loading...')),
     );
   }
 }
