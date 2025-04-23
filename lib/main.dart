87c4a610ddf4c3edbbb961587fe739f5db901e4f
PichamonM (MiN) <mintaconan4826@gmail.com>
2025-04-23 11:48:12 +0700

    add supabase setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:chatapp/screens/chat.dart';
 import 'package:chatapp/screens/splash.dart';
 import 'package:chatapp/screens/auth.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -10,13 +11,23 @@ import 'package:firebase_app_check/firebase_app_check.dart';
 // const Color.fromARGB(255, 63, 17, 177)
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // initialize Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  // Activate Firebase AppCheck
   await FirebaseAppCheck.instance.activate(
     webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
     androidProvider: AndroidProvider.debug,
     appleProvider: AppleProvider.appAttest,
   );
+
+  // Supabase setup
+  await Supabase.initialize( 
+    url: 'https://wegdusqykmpezrhnzzzu.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndlZ2R1c3F5a21wZXpyaG56enp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzMDgwMDgsImV4cCI6MjA2MDg4NDAwOH0.bD977z8IaTDn-HMrCnZylzrqzLgfZFdBByxncBmK0fs',
+  );
+
 
   runApp(const App());
 }
